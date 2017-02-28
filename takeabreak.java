f871a8e2c8d408ccd901f1220c3f60da640b683b
roguerammer999 <timheverett@gmail.com>
2017-02-28 18:44:22 -0500

    Update 2-28-17
    Several major changes on this update.
    - The cloud behavior is more responsive.  Clouds are still colored with gradients, but the colors differ based on the cloud height and the sun height.  A lower sun makes the gradient more second-color (lower color) dominant, so the clouds are mostly dark except for a sliver of light at the bottom.
    - When the sun position changes, the program no longer reloads a new set of cloud shapes/sizes/locations.  Instead, clouds reload only when the number of clouds changes.  Changing the sun position will update the colors.  I accomplished this by splitting "drawClouds" into "drawClouds" and "createClouds".
    - The sun and the sun's corona change size depending on position of sky just to appear brighter/darker.  The corona also changes color near the horizon.  The complex mechanics of the corona are split into a "drawCorona" method.
    - Slight tweaks to the colors of the background.
    - This is starting to get unwieldy.  The next update will probably split clouds into a separate class.
    - [Output image 1 (high sun, few clouds)](output_170226a.png)
    - [Output image 2 (low sun, many clouds)](output_170226b.png)

--- takeabreak.java
+++ takeabreak.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
-import java.awt.event.*;
+import java.util.ArrayList;
 
 public class takeabreak extends JFrame
 {
@@ -12,10 +12,10 @@ public class takeabreak extends JFrame
     
     private static JSlider cloudControl;
     private static JSlider sunControl;
-    private static int cloudCount;
+    private static int cloudCount = 6;
+    private static ArrayList allClouds = new ArrayList(25);
     private static int sunHeight;
-    private static GradientPaint skyGrad;
-    tbBackground mainBG = new tbBackground();
+    tbBackground mainBG;
     
     
     public takeabreak()
@@ -24,15 +24,16 @@ public class takeabreak extends JFrame
         //an output area of 1280x800 pixels.  The margins of the frame
         //require it to be somewhat larger.  I use 1280x800 to calculate
         //the location of the shapes.
-        this.setSize(1298,945);        
+        this.setSize(1298,945);
         this.setLocation(370,120);
         this.setTitle("Take a break on the beach!");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.add(mainBG, BorderLayout.CENTER);
         
-        
+        mainBG = new tbBackground();
         
         this.add(controls(), BorderLayout.SOUTH);
+        createClouds();
+        this.add(mainBG, BorderLayout.CENTER);
         
         this.setVisible(true);
     }
@@ -44,50 +45,53 @@ public class takeabreak extends JFrame
         public void paint(Graphics inputGr)
         {
             sunHeight = (int) (3.6 * (sunControl.getValue()));
-            cloudCount = cloudControl.getValue();
             Graphics2D baseGraphics = (Graphics2D)inputGr;
+                        
+            
+            //The sun is bigger when higher in the sky.
+            //The part of the sun diameter formula that multiplies by 2
+            //and divides by 12 is to ensure the diameter is always
+            //an even integer.
+            int sunDiameter = 60 + ( 2 * (int)(sunHeight)/12 );
+            Shape sun = new Ellipse2D.Float(
+                    (640-(sunDiameter/2)),
+                    390 - (sunDiameter/2) - sunHeight,
+                    sunDiameter, sunDiameter);
+            
+            //These three gradients/shapes correspond to the sky, water,
+            //and sand respectively.
+            GradientPaint skyGrad = (setSkyColors());
             Shape sky = new Rectangle2D.Float(0,0,1280,390);
-            Shape sun = new Ellipse2D.Float(610,360 - sunHeight,60,60);
-            Shape sunCorona = new Ellipse2D.Float(595,345 - sunHeight,90,90);
+            GradientPaint waterGrad = (setWaterColors());
             Shape water = new Rectangle2D.Float(0,390,1280,250);
+            GradientPaint sandGrad = (setSandColors());
             Shape sand = new Rectangle2D.Float(0,620,1280,180);
             
-            //These three gradients correspond to the sky, water, and sand
-            //respectively.
-            GradientPaint skyGrad = (setSkyColors(sunHeight));
-            GradientPaint waterGrad = (setWaterColors(sunHeight));
-            GradientPaint sandGrad = (setSandColors(sunHeight));
-            
-            //Drawing in the sky (it will be covered by the sun)
+            //Drawing the sky (it will be covered by the sun)
             baseGraphics.setPaint(skyGrad);
             baseGraphics.fill(sky);
             
-            //For the sun and "sunCorona" (a blur around the sun),
-            //I arbitrarily decided to define the gradient right when
-            //filling the shape.
-            baseGraphics.setPaint(new GradientPaint(
-                    0,305,new Color(184,172,148),
-                    0,350,new Color(166,153,134)));
-            baseGraphics.setComposite(AlphaComposite.getInstance
-                (AlphaComposite.SRC_OVER, 0.37F));
-            baseGraphics.fill(sunCorona);
+            drawCorona(baseGraphics);
+            
+            //Drawing the sun
             baseGraphics.setComposite(AlphaComposite.getInstance
                 (AlphaComposite.SRC_OVER, 1F));
-            baseGraphics.setPaint(new GradientPaint(
-                0,360,new Color(255,244,228),
-                0,390,new Color(227,215,176)));
+            baseGraphics.setColor(Color.WHITE);
             baseGraphics.fill(sun);
             
-            //Drawing in the water and sand over the sun
+            //Drawing the water and sand over the sun/corona
             baseGraphics.setPaint(waterGrad);
             baseGraphics.fill(water);
             baseGraphics.setPaint(sandGrad);
             baseGraphics.fill(sand);
             
-            
             drawClouds(baseGraphics);
         }
+        
     }
+    
+    
+    
     
     //The "controls" panel currently has two sliders, one to control
     //the number of clouds and the other to set the height of the sun
@@ -98,7 +102,7 @@ public class takeabreak extends JFrame
         JPanel tbControls = new JPanel();
         
         JLabel cloudLabel = new JLabel ("Number of clouds");
-        cloudControl = new JSlider(0,25,7);
+        cloudControl = new JSlider(0,25,cloudCount);
         cloudControl.setMajorTickSpacing(5);
         cloudControl.setMinorTickSpacing(1);
         cloudControl.setPaintTicks(true);
@@ -129,21 +133,31 @@ public class takeabreak extends JFrame
         
     }
     
+    //This class will represent ships on the water.
+    private class ship extends JComponent
+    {
+        
+    }
+    
+    
+    
     //This method sets linear color changes for the gradient colors of the sky.
-    private static GradientPaint setSkyColors (int sunHeight)
+    //The "1" colors are the top color of the gradient, "2" are bottom.
+    private static GradientPaint setSkyColors ()
     {
         int red1 = 62 + (sunHeight/4);
         int green1 = 140 + (sunHeight/6);
         int blue1 = 210 + (sunHeight/10);
-        int red2 = 210 - (sunHeight/10);
-        int green2 = 132 + (sunHeight/4);
+        int red2 = 230 - (sunHeight/10);
+        int green2 = 136 + (sunHeight/4);
         int blue2 = 51 + (sunHeight/2);
         return new GradientPaint(0,0,new Color(red1,green1,blue1),
                 0,390,new Color(red2,green2,blue2));
     }
     
     //This sets linear color changes for the water color gradient.
-    private static GradientPaint setWaterColors (int sunHeight)
+    //The "1" colors are the top color of the gradient, "2" are bottom.
+    private static GradientPaint setWaterColors ()
     {
         int red1 = 15 + (sunHeight/8);
         int green1 = 45 + (sunHeight/4);
@@ -156,81 +170,135 @@ public class takeabreak extends JFrame
     }
     
     //This sets linear color changes for the sand color gradient.
-    private static GradientPaint setSandColors (int sunHeight)
+    private static GradientPaint setSandColors ()
     {
-        int red1 = 180 + sunHeight/6;
-        int green1 = 111 + sunHeight/3;
-        int blue1 = 56 + sunHeight/3;
-        int red2 = 100 + sunHeight/3;
-        int green2 = 65 + (int)(sunHeight/2.5);
-        int blue2 = 35 + sunHeight/3;
+        int red1 = 173 + sunHeight/6;
+        int green1 = 106 + sunHeight/3;
+        int blue1 = 45 + sunHeight/3;
+        int red2 = 89 + sunHeight/3;
+        int green2 = 58 + (int)(sunHeight/2.5);
+        int blue2 = 27 + sunHeight/3;
         return new GradientPaint(0,640,new Color(red1,green1,blue1),
                 0,800,new Color(red2,green2,blue2));
     }
     
-    //This class represent simple oval clouds in the air by providing
-    //height, width, upper left corner x/y-coordinates, and color shade.
-    //It randomizes "xLRCorner" and "yLRCorner" as x- and y-coordinates
-    //for the lower right corner of the cloud, then randomizes width
-    //and height for 175 and 40 max, respectively.
+    
+    
+    //This class represents simple oval clouds in the air.  It defines
+    //dimensions and location, as well as two sets of RGB values for a gradient,
+    //one set for the top part of the cloud and one for the bottom.
     private static int [] cloud ()
     {
-        int xLRCorner = (int)(Math.random()*1280);
-        int yLRCorner = (int)(Math.random()*330);
-        int width = 55 + (int)(Math.random()*175);
-        
-        //This block ensures that the program does not attempt to draw
-        //anything outside of the boundaries (e.g. negative coordinates)
-        if(width > xLRCorner)
-            width = xLRCorner;
-        int height = 20 + (int)(Math.random()*40);
-        if(height > yLRCorner)
-            height = yLRCorner;
-        if(height<10)
-            height = 10;
-        if((float)width/(float)height < 3)
-            width = (int)(2.5 * height);
-        if((float)width/(float)height > 9)
-            width = 8 * height;
-        
+        //Location/dimensions are semi-randomized.  The width is 175-230,
+        //and height is 2.7 to 6.7 times less.  Clouds near the horizon
+        //can have less width and can be flatter.
+        int xLRCorner = 185 + (int) (Math.random()*1095);
+        int yLRCorner = 30 + (int) (Math.random()*320);
+        int width = 70 + (int)
+                (Math.random() *
+                (185-(yLRCorner/5))
+                );
+        int height = (int)
+                (width /
+                (2.7 + (Math.random()*4) + (yLRCorner/150))
+                );
+
         //With the size boundaries and lower right corners set,
-        //The upper left corner's coordinates are calculated.
+        //the upper left corner's coordinates are calculated.
         int xULCorner = xLRCorner - width;
         int yULCorner = yLRCorner - height;
         
-        //Color shade is determined on height.  A cloud higher in the sky
-        //will be darker.
-        int cloudDarkness = 250 - (int)(yULCorner/6);
-        return new int [] {xULCorner, yULCorner, width, height,
-        cloudDarkness};
+        return new int [] {xULCorner, yULCorner, width, height};
     }
     
-    //This class will represent ships on the water.
-    private class ships extends JComponent
+    //The createClouds method redraws cloud locations.  If it is not used,
+    //cloud colors of existing shapes can be changed without draiwng new shapes.
+    private static void createClouds()
     {
-        
+        allClouds.clear();
+        for(int counter = 0; counter < cloudCount; counter++)
+        {
+            int [] cloudComponent = cloud();
+            allClouds.add(cloudComponent);
+        }
     }
     
-    //This takes a cloud object (an int array) and creates an ellipse
-    //using the height/width/coordinate parameters, then fills it
-    //with the color but slightly blue/red tint.
+    
+    //This draws the sun's corona based on the height of the sun.  A higher sun
+    //results in a larger, brighter corona.  At sunset, the corona diminishes
+    //dramatically.
+    private static void drawCorona(Graphics inputGr)
+    {
+        Graphics2D coroGraphics = (Graphics2D) inputGr;
+        //The part of the corona diameter formula that multiplies by 2
+        //and divides by 12 is to ensure the diameter is always an even integer.
+        int coronaDiam = 100 + ( 2 * (int)(sunHeight-30)/12 );
+        //The "if" condition causes the corona to shrink dramatically
+        //as the sun becomes hidden by the horizon.
+        if(sunHeight<=30)
+            coronaDiam = 60 + (int)(sunHeight/3 * 4);
+        int coronaX = 640 - (coronaDiam/2);
+        int coronaY = 390 - (coronaDiam/2);
+        Shape sunCorona = new Ellipse2D.Float(coronaX,coronaY - sunHeight,
+                coronaDiam,coronaDiam);
+        Color coronaColor = new Color(
+                243 + (sunHeight/30),
+                237 + (sunHeight/20),
+                255 -   (int)(180 / ( (sunHeight/20) + 1)));
+        
+        coroGraphics.setColor (coronaColor);
+        coroGraphics.setComposite(AlphaComposite.getInstance
+                (AlphaComposite.SRC_OVER, 0.9F));
+            coroGraphics.fill(sunCorona);
+    }
+    
+    
+    //This takes a cloud object (an int array stored in ArrayList cloudCount)
+    //and creates an ellipse using the height/width/coordinate parameters.
     private static void drawClouds(Graphics inputGr)
     {
         Graphics2D cloudDraw = (Graphics2D) inputGr;
-        cloudCount = cloudControl.getValue();
         for(int counter = 0; counter < cloudCount; counter++)
         {
-            int [] newCloud = cloud();
+            int [] newCloud = (int []) allClouds.get(counter);
             Shape ovalCloud = new Ellipse2D.Float(newCloud[0],
                     newCloud[1],newCloud[2],newCloud[3]);
-            cloudDraw.setColor(
-                    new Color(newCloud[4]-4, newCloud[4]-10, newCloud[4]));
+            
+            //These formulas change the cloud colors based on height over
+            //horizon of cloud ("yHT") and sun (sunHeight).  "top" variables
+            //are RGB values of the top half of the cloud, with "bottom"
+            //as the bottom half.  These form a gradient.
+            //Maximum yHt is 340, max sunHeight is 360
+            int yHt = newCloud[1];
+            int topR = (int) (115 - (yHt/10) + (sunHeight * 130/360));
+            int topG = (int) (100 - (yHt/10) + (sunHeight * 150/360));
+            int topB = (int) (120 - (yHt/12) + (sunHeight * 130/360));
+            int bottomR = (int) (225 + (yHt/12) - (sunHeight / 30));
+            int bottomG = (int) (210 + (yHt/10) - (sunHeight / 30));
+            int bottomB = (int) (145 + (yHt/12) + (sunHeight / 5));
+
+
+            //This draws the gradient.  When the sun is high in the sky,
+            //the gradient starts at the top of the cloud.  A lower sun
+            //has the gradient start lower than the top, with a maximum of
+            //40% down.  At sunset, clouds are top color dominant.
+            cloudDraw.setPaint(new GradientPaint(
+                newCloud[0],
+                    newCloud[1] + (float)
+                            //formula for cloud darkening
+                            ( (0.4F * newCloud[3]) * ((360F-sunHeight))/360F),
+                    new Color(topR,topG,topB),
+                newCloud[0],
+                    newCloud[1] + newCloud[3],
+                    new Color(bottomR,bottomG,bottomB)));
             cloudDraw.fill(ovalCloud);
         }
     }
     
     private void cloudSlideChange ()
     {
+        cloudCount = cloudControl.getValue();
+        createClouds();
         mainBG.repaint();
     }
     
